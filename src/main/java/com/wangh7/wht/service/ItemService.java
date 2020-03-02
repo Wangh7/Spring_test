e4878f83377d084a47ebdf682ad435f633a4c7f3
Wangh7 <978102365@qq.com>
2020-03-03 00:33:51 +0800

    添加了商品DAO和Service

---
+++ src/main/java/com/wangh7/wht/service/ItemService.java
@@ -0,0 +1,28 @@
+package com.wangh7.wht.service;
+
+
+import com.wangh7.wht.dao.ItemDAO;
+import com.wangh7.wht.pojo.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.data.domain.Sort;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+    @Autowired
+    ItemDAO itemDAO;
+
+    public List<Item> list() { //获取所有图书
+        return itemDAO.findAll(Sort.by(Sort.Direction.DESC, "item_id"));
+    }
+
+    public void addOrUpdate(Item item) {
+        itemDAO.save(item);
+    }
+
+    public void deleteById(int id) {
+        itemDAO.deleteById(id);
+    }
+}
