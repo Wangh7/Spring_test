4b54e45d4f0388fa25b4308f2674371c95310197
Wangh7 <978102365@qq.com>
2020-04-24 17:57:24 +0800

    修改了查询最近上架商品时不能显示仅上架1件的商品

--- src/main/java/com/wangh7/wht/dao/ItemStockDAO.java
+++ src/main/java/com/wangh7/wht/dao/ItemStockDAO.java
@@ -17,6 +17,6 @@ public interface ItemStockDAO extends JpaRepository<ItemStock, Integer> {
     @Query(value = "select new com.wangh7.wht.entity.HotSell(a.itemType.typeId ,a.itemType.typeName ,a.itemType.typeCode ,count(a.itemId)) from ItemStock a where a.status = 'Y' group by a.itemType.typeId order by count(a.itemId) desc")
     List<HotSell> getHotSell();
 
-    @Query(value = "select new com.wangh7.wht.entity.HotSell(a.itemType.typeId, a.itemType.typeName, a.itemType.typeCode) from ItemStock a where a.status = 'N' group by a.itemType.typeId having(count(a.itemType.typeId)>1)")
+    @Query(value = "select new com.wangh7.wht.entity.HotSell(a.itemType.typeId, a.itemType.typeName, a.itemType.typeCode) from ItemStock a where a.status = 'N' group by a.itemType.typeId having(count(a.itemType.typeId)>=1)")
     List<HotSell> getRecentItem();
 }
